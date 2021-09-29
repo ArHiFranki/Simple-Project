7e114e77468462161ecc5aee50454e3a73a44322
ArHiFranki <franki550@gmail.com>
2021-09-29 17:58:24 +0700

    Написал скрипт GameController содержащий основные параметры состояния игры. Добавил объект GameController на сцену.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] private Menu _menu;
     [SerializeField] private bool _isGamePause;
+    [SerializeField] private bool _isGameOver;
+    [SerializeField] private bool _isPreparationPhase;
+    [SerializeField] private bool _isFightPhase;
+    [SerializeField] private int _baseHitPointsMax;
+    [SerializeField] private int _gold;
+
+    private int _curruntBaseHitPoints;
 
     public bool IsGamePause => _isGamePause;
+    public bool IsPreparationPhase => _isPreparationPhase;
+    public bool IsFightPhase => _isFightPhase;
+    public int BaseHitPoints => _curruntBaseHitPoints;
+    public int Gold => _gold;
+
+    public event UnityAction GameOver;
+    public event UnityAction<int> GoldChanged;
 
     private void OnEnable()
     {
@@ -19,8 +34,58 @@ public class GameController : MonoBehaviour
         _menu.GamePause -= SetGamePauseCondition;
     }
 
+    private void Start()
+    {
+        _isGameOver = false;
+        _isPreparationPhase = true;
+        _isFightPhase = false;
+        _curruntBaseHitPoints = _baseHitPointsMax;
+    }
+
     public void SetGamePauseCondition(bool condition)
     {
         _isGamePause = condition;
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        _curruntBaseHitPoints -= damage;
+
+        if (_curruntBaseHitPoints <= 0)
+        {
+            _isGameOver = true;
+            GameOver?.Invoke();
+        }
+    }
+
+    public void AddHitPoints(int value)
+    {
+        if (_curruntBaseHitPoints < _baseHitPointsMax)
+        {
+            _curruntBaseHitPoints += value;
+
+            if (_curruntBaseHitPoints > _baseHitPointsMax)
+            {
+                _curruntBaseHitPoints = _baseHitPointsMax;
+            }
+        }
+    }
+
+    public void ChangeGold(int goldValue)
+    {
+        _gold += goldValue;
+        GoldChanged?.Invoke(_gold);
+    }
+
+    public void SetPreparationPhase()
+    {
+        _isPreparationPhase = true;
+        _isFightPhase = false;
+    }
+
+    public void SetFightPhase()
+    {
+        _isPreparationPhase = false;
+        _isFightPhase = true;
     }
 }
