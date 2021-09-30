b6128fef918328b9055f7330ac22c3df25f76ef0
ArHiFranki <franki550@gmail.com>
2021-09-30 13:39:13 +0700

    Добавил отображение количества золота в UI

---
+++ Assets/Scripts/UI/GoldDisplay.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GoldDisplay : MonoBehaviour
+{
+    [SerializeField] private GameController _gameController;
+    [SerializeField] private TMP_Text _goldCount;
+
+    private void OnEnable()
+    {
+        _goldCount.text = "Gold: " + _gameController.Gold.ToString();
+        _gameController.GoldChanged += OnGoldChanged;
+    }
+
+    private void OnDisable()
+    {
+        _gameController.GoldChanged -= OnGoldChanged;
+    }
+
+    private void OnGoldChanged(int money)
+    {
+        _goldCount.text = "Gold: " + _gameController.Gold.ToString();
+    }
+}
